0d8241662f1562c7d65df150b58255254aed0a93
thenightx2003-png <thenightx2003@gmail.com>
2026-09-24 14:36:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
 # Đường dẫn file dữ liệu dùng chung trên máy chủ
 CSV_FILE = "history.csv"
  
-# Thực đơn cố định của nhà hàng Mr. Bình
+# Thực đơn cố định của nhà hàng KIMOCHI
 menu = {
    "Đồ ăn": {
        "Pizza Hải Sản": 150000,
